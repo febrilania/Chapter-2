10d7bb7e82f4c2ebaed3ebb0a488f0905a6576d1
unknown <febrilanianurislami@gmail.com>
2023-11-28 02:12:53 +0700

    update day 12

--- index.js
+++ index.js
@@ -17,12 +17,19 @@ app.get("/add-myproject", addprojectView);
 
 app.post("/add-myproject", addproject);
 
+app.post("/delete-myproject/:id", deleteProject);
+
+app.get("/update-project/:id", updateProjectview);
+app.post("/update-project", updateProject);
+
 app.get("/detail-project/:id", detailproject);
 
 app.get("/contact", contact);
 
+const data = [];
+
 function home(req, res) {
-  res.render("index");
+  res.render("index", { data });
 }
 
 function addprojectView(req, res) {
@@ -30,30 +37,61 @@ function addprojectView(req, res) {
 }
 
 function addproject(req, res) {
-  const name = req.body.name;
-  const startDate = req.body.startDate;
-  const endDate = req.body.endDate;
-  const description = req.body.description;
+  const { name, startDate, endDate, description } = req.body;
 
-  console.log("Nama Project :", name);
-  console.log("Tanggal Mulai : ", startDate);
-  console.log("Tanggal Selesai : ", endDate);
-  console.log("Deskripsi : ", description);
+  // console.log("Nama Project :", name);
+  // console.log("Tanggal Mulai : ", startDate);
+  // console.log("Tanggal Selesai : ", endDate);
+  // console.log("Deskripsi : ", description);
 
-  res.redirect("add-myproject");
+  const dataProject = { name, startDate, endDate, description };
+
+  data.unshift(dataProject);
+
+  res.redirect("/");
+}
+
+function deleteProject(req, res) {
+  const { id } = req.params;
+  data.splice(id, 1);
+
+  res.redirect("/");
+}
+
+function updateProjectview(req, res) {
+  const { id } = req.params;
+  const dataFilter = data[parseInt(id)];
+  dataFilter.id = parseInt(id);
+  res.render("update-project", { data: dataFilter });
+}
+
+function updateProject(req, res) {
+  const { id, name, startDate, endDate, description } = req.body;
+
+  // console.log("Nama Project :", name);
+  // console.log("Tanggal Mulai : ", startDate);
+  // console.log("Tanggal Selesai : ", endDate);
+  // console.log("Deskripsi : ", description);
+
+  // const dataProject = { id, name, startDate, endDate, description };
+
+  // data.unshift(dataProject);
+
+  data[parseInt(id)] = {
+    name,
+    startDate,
+    endDate,
+    description,
+  };
+
+  res.redirect("/");
 }
 
 function detailproject(req, res) {
-  const id = req.params.id;
-
-  const data = {
-    id,
-    project: "Project 1",
-    start: "21-2-2023",
-    end: "26-2-2023",
-    deskripsi: "content 1",
-  };
-  res.render("detail-project", { data });
+  const { id } = req.params;
+  const dataFilter = data[parseInt(id)];
+  dataFilter.id = parseInt(id);
+  res.render("detail-project", { data: dataFilter });
 }
 
 function contact(req, res) {
